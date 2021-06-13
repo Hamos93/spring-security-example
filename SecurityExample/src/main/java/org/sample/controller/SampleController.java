88ad799b0dd2c34cfb2b98fe89430215c7b7a7fd
Hamos93 <gksmfwm00@naver.com>
2021-06-13 18:54:52 +0900

    어노테이션을 이용한 스프링 시큐리티 설정

--- SecurityExample/src/main/java/org/sample/controller/SampleController.java
+++ SecurityExample/src/main/java/org/sample/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.sample.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
+	// XML을 이용하는 스프링 시큐리티 설정
 	@GetMapping("/all")
 	public void doAll() {
 		log.info("do all can access everybody");
@@ -24,6 +27,19 @@ public class SampleController {
 	@GetMapping("/admin")
 	public void doAdmin() {
 		log.info("admin only");
+	}
+	
+	// 어노테이션을 이용하는 스프링 시큐리티 설정
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+	@GetMapping("/annoMember")
+	public void doMember2() {
+		log.info("logined annotation member");
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		log.info("admin annotation only");
 	}
 	
 }
